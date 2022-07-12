4c60d554b1f9931c7decdf4e04dac7f29d25b914
mihals <mihalsnes@gmail.com>
2022-07-12 16:49:20 +0300

    Lint block added

--- app/src/main/java/ru/netology/nmedia/ui/FeedFragment.kt
+++ app/src/main/java/ru/netology/nmedia/ui/FeedFragment.kt
@@ -3,25 +3,16 @@ package ru.netology.nmedia.ui
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.Gravity.apply
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.GravityCompat.apply
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResultListener
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.google.gson.Gson
-import ru.netology.nmedia.Post
 import ru.netology.nmedia.R
-import ru.netology.nmedia.ScrollPostFragment
-import ru.netology.nmedia.ScrollPostFragmentDirections
 import ru.netology.nmedia.adapter.PostAdapter
 import ru.netology.nmedia.databinding.FeedFragmentBinding
 import ru.netology.nmedia.viewModel.PostViewModel
-import java.lang.ProcessBuilder.Redirect.to
 
 class FeedFragment : Fragment() {
 
